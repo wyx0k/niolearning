dde420886e6579e73a9864157357e7cea96f04bb
wyx0k <wangyxokxok@126.com>
2018-11-18 23:58:16 +0800

    新增服务端和客户端的最小启动逻辑,修改了maven配置的java版本为1.8.

--- src/main/java/indi/wyx0k/nio/TestClient.java
+++ src/main/java/indi/wyx0k/nio/TestClient.java
@@ -1,4 +1,41 @@
 package indi.wyx0k.nio;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
 public class TestClient {
+    public static void main(String[] args) {
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(workerGroup)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+                    }
+                });
+        connect(bootstrap,"localhost",8080,1);
+    }
+    private static void connect (final Bootstrap b, final String host, final int port, final int tryTimes){
+
+        b.connect(host,port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if(future.isSuccess()){
+                    System.out.println("连接服务器成功:-->"+host+":"+port);
+                }else{
+                    if(tryTimes <=5){
+                        System.out.println("连接失败,第"+tryTimes+"次尝试重连中...");
+                        connect(b,host,port,tryTimes+1);
+                    }else{
+                        System.out.println("连接失败");
+                    }
+                }
+            }
+        });
+    }
 }
--- src/main/java/indi/wyx0k/nio/TestServer.java
+++ src/main/java/indi/wyx0k/nio/TestServer.java
@@ -1,11 +1,41 @@
 package indi.wyx0k.nio;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
 public class TestServer {
     public static void main(String[] args) {
-        ok();
-        System.out.print(ok()[1]);
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        final ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(bossGroup,workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 512)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+            protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+
+            }
+        });
+
+        bind(serverBootstrap,8080);
     }
-    public static int[] ok(){
-        return new int[]{1,2};
+    private static void bind (final ServerBootstrap sb, final int port){
+        sb.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if(future.isSuccess()){
+                    System.out.println("端口: "+port+"成功");
+                }else{
+                    System.out.println("端口: "+port+"启动失败,正在尝试"+(port+1));
+                    bind(sb,port+1);
+                }
+            }
+        });
     }
 }
